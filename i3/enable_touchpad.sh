f85985ab16af2e71707690509b84f5f2b818171c
Achille0072 <achille0072.mail@gmail.com>
2024-12-24 20:26:22 +0100

    Removed debug messages

--- i3/enable_touchpad.sh
+++ i3/enable_touchpad.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -ex
 xinput set-prop "$(xinput | grep -i touchpad | awk '{ print $3, $4, $5 }')" "libinput Tapping Enabled" 1
 xinput set-prop "$(xinput | grep -i touchpad | awk '{ print $3, $4, $5 }')" "libinput Disable While Typing Enabled" 0
 
